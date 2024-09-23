93b2696460836ed932688ff62fa940ceb91912a1
HungLam2801 <lamxuanhung01282004@gmail.com>
2024-09-23 08:00:35 +0700

    Register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DestinationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
